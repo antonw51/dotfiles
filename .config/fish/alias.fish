4c1db847d546ff0f36c467714f8b439e248251c4
Anton <aligator.h0sptial.e@gmail.com>
2025-01-01 01:56:55 +0100

    add git reference alias

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -15,4 +15,6 @@ alias sv 'layout -t se'
 # SSH
 alias louis 'echo "$(set_color red)You changed this dimwit, use \'vpn\' instead"'
 
+# Git
 alias glog 'git log --graph --abbrev-commit --decorate --format=format:\'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)\''
+alias gref 'git show -s --pretty=reference '
